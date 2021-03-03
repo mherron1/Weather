34f6fb4b49b958661dc685706146276ba3d2287a
Michael Herron <mherron2021@gmail.com>
2021-03-03 21:15:17 +0000

    added 4 day forecast

--- app.js
+++ app.js
@@ -1,29 +1,80 @@
 
-let forecast = {};
+function Forecast(location, temp, description, wind){
+    this.location = location,
+    this.temperature = temp,
+    this.description = description,
+    this.wind = wind
+}
 
-function newForecast(){
-    let place = document.getElementById("myText").value;
+function DailyForecast(temp, description, wind){
+    this.temperature = temp,
+    this.description = description,
+    this.wind = wind
+}
+
+
+let place = "";
+let daily = [];
+
+let button1 = document.querySelector("#getCurrentWeather");
+button1.addEventListener("click", getCurrentWeather)
+
+let button2 = document.querySelector("#fourDay");
+button2.addEventListener("click", getFourDayForecast)
+
+
+function getCurrentWeather(){
+    place = document.getElementById("myText").value;
+    if(place === ""){ place = "ireland"}
     const getWeather =  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&units=metric&APPID=845d119a7ee8a96fc71e301e1235b7be`)
 
     getWeather
-       .then((result)=>{
-         result.json(result)
-        .then((result)=>{    
-        forecast = {
-            "location": result.name,
-            "temperature": result.main.temp,
-            "description" : result.weather[0].description,
-            "windSpeed" : result.wind.speed
-                }
-        displayForecast()    
+       .then(data =>{
+         return data.json(data)
+
+       }).then(data =>{    
+        
+         let forecast = new Forecast(data.name, data.main.temp, data.weather[0].description, data.wind.speed)
+         displayForecast(forecast)
+
        })
-       .catch((err)=> console.log("Oops, something went wrong. " +  err ))   
-})
 }
 
 
 
-function displayForecast(){
+function getFourDayForecast(){
+    
+    daily = [];
+
+    place = document.getElementById("myText").value;
+    if(place === ""){ place = "ireland"}
+    const getFourDay =  fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${place}&cnt=4&units=metric&APPID=845d119a7ee8a96fc71e301e1235b7be`)
+
+    getFourDay
+       .then(data =>{
+         return data.json(data)
+
+       }).then(data =>{    
+
+           data.list.forEach(item =>{
+           let dayForecast = new DailyForecast(item.main.temp, item.weather[0].description, item.wind.speed)
+           daily.push(dayForecast)
+
+           })
+
+           console.log(daily)
+           displayThreeDays()
+            
+    
+       })
+}
+
+
+
+
+
+function displayForecast(forecast){
+    document.querySelector("#weather").style.display = "block";
     let weather = document.querySelector("#weather");
     while (weather.firstChild) {
         weather.removeChild(weather.firstChild);
@@ -34,14 +85,50 @@ function displayForecast(){
      let location = document.createElement("p")
      location.textContent = `Forecast for ${forecast.location}`;
      weather.appendChild(location);
-     let disc = document.createElement("p")
-     disc.textContent = `Summary: ${forecast.description}`;
-     weather.appendChild(disc);
+
+     let discr = document.createElement("p")
+     discr.textContent = `Summary: ${forecast.description}`;
+     weather.appendChild(discr);
+
      let temp = document.createElement("p")
      temp.textContent = `Temperature: ${forecast.temperature}`;
      weather.appendChild(temp);
 
      let wind = document.createElement("p")
-     wind.textContent = `Wind Speed: ${forecast.windSpeed}`;
+     wind.textContent = `Wind Speed: ${forecast.wind}`;
      weather.appendChild(wind);
+}
+
+
+
+function displayThreeDays(){
+    let container = document.querySelector("#threeDayContainer")
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+
+
+    for (let i=0; i< daily.length; i++ ){
+
+ 
+    let day = document.createElement("div")
+    day.classList = "dailyForecast"
+    container.appendChild(day)
+
+    let descr = document.createElement("p")
+    descr.textContent = daily[i].description;
+    day.appendChild(descr)
+
+
+    let temp = document.createElement("p")
+    temp.textContent = daily[i].temperature;
+    day.appendChild(temp)
+
+    let wind = document.createElement("p")
+    wind.textContent = daily[i].wind;
+    day.appendChild(wind)
+
+
+
+    }
 }
